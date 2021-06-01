10d928e3df753d6aba7d4e1f0e1e712fedc801b6
Anirudh Mahajan <anirudhmahajan18@gmail.com>
2021-06-01 19:44:01 +0530

    Added Contact Success Feedback

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,6 +3,8 @@ import "../Stylesheets/Contact.css";
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import emailjs from "emailjs-com";
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 class Contact extends React.Component {
 
@@ -12,24 +14,36 @@ class Contact extends React.Component {
             name: '',
             email: '',
             subject: '',
-            message: ''
+            message: '',
+            open: false,
         }
     }
 
     submitForm = () => {
-        emailjs.send("service_9dglmz6","template_xi7wqn1",{
+        emailjs.send("service_9dglmz6", "template_xi7wqn1", {
             name: this.state.name,
             email: this.state.email,
             subject: this.state.subject,
             message: this.state.message,
-            }, "user_E5VtFEB47Kfk24ddpkhu8");
+        }, "user_E5VtFEB47Kfk24ddpkhu8");
 
         this.setState({
             name: '',
             email: '',
             subject: '',
-            message: ''
+            message: '',
+            open: true,
         })
+    }
+
+    handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+          }
+      
+          this.setState({
+              open: false,
+          });
     }
 
     render() {
@@ -58,6 +72,13 @@ class Contact extends React.Component {
                         <p className="buttonMargin"></p>
                     </form>
                 </div>
+                <Snackbar 
+                        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                        open={this.state.open} autoHideDuration={5500} onClose={this.handleClose}>
+                    <MuiAlert onClose={this.handleClose} severity="success" variant="filled" elevation="6">
+                        Email Sent Successfully!
+                </MuiAlert>
+                </Snackbar>
             </div>
         )
     }
