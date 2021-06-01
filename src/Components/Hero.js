ed52acc15a58b3af142a07fe27a1bb53f6b21e6d
Anirudh Mahajan <anirudhmahajan18@gmail.com>
2021-06-01 20:09:16 +0530

    Changed Hero Text

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -12,7 +12,7 @@ class Hero extends React.Component{
                 </div>
                 <div className="intro">
                     <h1 className="name">Anirudh Mahajan</h1>
-                    <h3 className="headline">I build amazing mobile and web apps.<br/>With a passion to serve great user experiences!</h3>
+                    <h3 className="headline">I craft amazing mobile and web apps.<br/>With a passion to serve great user experiences!</h3>
                     <a href="/Resume.pdf" className="downloadLink" download>Download My Resume!</a>
                 </div>
             </div>
